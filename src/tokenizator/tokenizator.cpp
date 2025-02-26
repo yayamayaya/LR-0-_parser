7636e5fbda0e70e450b5b399fde22ff13e4a0fb7
yayamayaya <semenov.aleksandr@phystech.edu>
2025-02-26 15:37:01 +0300

    написал класс токенизатора для нашего синтаксиса

---
+++ src/tokenizator/tokenizator.cpp
@@ -0,0 +1,92 @@
+#include "tokenizator.hpp"
+#include "debugging.hpp"
+
+#include <iostream>
+#include <assert.h>
+#include <string>
+#include "debugging.hpp"
+#include "tokenizator.hpp"
+
+ret_status tokenizator::run(token_t *&token_arr, const std::string expression)
+{
+    _RETURN_ON_TRUE(!expression.c_str(), NO_ARGV_STRING_ERR, LOG("> no argument given\n"));
+
+    LOG("> starting tokenization...\n");
+    
+    LOG("> initialising token array\n");
+    tokens_arr tokens = {};
+
+    LOG("> starting tokens detection\n");
+    for (unsigned int pos = 0; pos < expression.size();)
+    {
+        token_t token = detect_token(expression, pos);
+        if (token.type == SPACE_SYMB) 
+            continue;
+
+        if (token.type == SYNTAX_NOT_DET) 
+            return SYNTAX_ERR;
+
+        if (token.type != SPACE_SYMB)
+        {
+            _RETURN_ON_TRUE(tokens.add_token(token), MEM_RLC_ERR);
+
+            continue;
+        }
+
+        return FATAL_ERR;
+    }
+
+    tokens.token_dump();
+    LOG("> tokenization is finished\n");
+
+    token_arr = tokens.return_data();
+
+    return 0;
+}
+
+inline token_t tokenizator::detect_token(const std::string &buff, unsigned int &pos)
+{
+    token_t token = {};
+
+    LOG("> current pos: " << pos << ", the rest is: " << buff.data() + pos << "\n");
+
+    if (isspace(*(buff.data() + pos)))
+    {
+        pos++;
+
+        token.type = SPACE_SYMB;
+        return token;
+    }
+
+    std::unordered_map<char, Operations>::const_iterator iter = operations.find(*(buff.data() + pos));
+    if (iter != operations.end())
+    {
+        LOG("> operation: '" << (unsigned int)iter->second << "' found\n");
+
+        pos++;
+        
+        token.data.operation = iter->second;
+        token.type = OPERATION;
+
+        return token;
+    }    
+    
+    int n = 0;
+    number_t number = 0;
+    sscanf(buff.data() + pos, "%lf%n", &number, &n);
+    if (n)
+    {
+        LOG("> number '" << number << "' was found\n");
+
+        pos += (unsigned int)n;
+
+        token.data.number = number;
+        token.type = NUMBER;
+        return token;
+    }
+
+    LOG("> couldn't detect token, syntax error\n");
+    token.type = SYNTAX_NOT_DET;
+    return token;
+}
+
---
+++ src/tokenizator/tokenizator.hpp
@@ -0,0 +1,116 @@
+#ifndef TOKENIZATOR_H
+#define TOKENIZATOR_H
+
+#include "syntax.hpp"
+#include <unordered_map>
+#include <string>
+#include <algorithm>
+
+enum Tokenizator_error
+{
+    MEM_RLC_ERR         = 1011,
+    NO_ARGV_STRING_ERR  = 1012,
+    SYNTAX_ERR          = 1013,
+
+    FATAL_ERR           = 404,
+
+};
+
+using number_t = double;
+
+union token_data
+{
+    token_data() {};
+
+    token_data(number_t num):    number(num) {};
+
+    token_data(Operations op): operation(op) {}; 
+
+    ~token_data() {};
+
+    number_t number;
+
+    Operations operation;
+};
+
+struct token_t
+{
+    token_t(): data(), type() {};
+    ~token_t() {};
+
+    token_data data;
+    Data_types type;
+};
+
+class tokenizator
+{
+public:
+
+    ret_status run(token_t *&token_arr, const std::string expression);
+
+private:
+
+    inline token_t detect_token(const std::string &buff, unsigned int &pos);
+
+    std::unordered_map<char, Operations> operations =
+    {
+        {'+', PLUS},
+        {'-', MINUS},
+        {'*', UMNOZH},
+        {'/', DELIT}
+    };
+
+    class tokens_arr
+    {
+    public:
+
+        tokens_arr(): tokens(new token_t[init_size]), array_size(init_size), token_num(0) {};
+        
+        ~tokens_arr () 
+        {
+            delete [] tokens;
+        };
+        
+        ret_status      add_token(const token_t token);
+        
+        unsigned long   tokens_num() {return token_num;}
+        
+        token_t        *return_data();
+        
+        void            token_dump();
+        
+        private:
+        
+        static constexpr long int init_size = 100;
+        
+        token_t      *tokens;
+        unsigned long array_size;
+        unsigned long token_num;
+        
+        ret_status reallc_up()
+        {
+            array_size *= 2;
+            token_t *arr_holder = new token_t[array_size];
+            if (!arr_holder) return MEM_RLC_ERR;
+            
+            std::copy(tokens, tokens + token_num, arr_holder);
+            
+            delete [] tokens;
+            
+            tokens = arr_holder;
+            return 0;
+        }
+        
+        tokens_arr(const tokens_arr& copy): tokens(NULL), array_size(0), token_num(0) {(void) copy;};
+        
+        tokens_arr& operator=(const tokens_arr& copy)
+        {
+            (void) copy;
+            
+            return *this;
+        }
+    };
+
+};  
+
+#endif
