b5c9a6ee8c7f68020f991ae73cf175c21f9c372e
yayamayaya <semenov.aleksandr@phystech.edu>
2025-03-04 02:14:22 +0300

    убрал ненужный енам, дополнил нужный енам

--- src/common/syntax.hpp
+++ src/common/syntax.hpp
@@ -20,13 +20,20 @@ using ret_status = int;
 enum Syntax_names
 {
     ID        = 0x01,
-    SUM       = 0x60,
-    MULT      = 0x70,
+
+    SUM       = 0x20,
+    PLUS      = 0x21,
+    MINUS     = 0x22,
+
+    MULT      = 0x40,
+    UMNOZH    = 0x41,
+    DELIT     = 0x42,
+
     OP_BRACK  = 0x04,
     CL_BRACK  = 0x05,
+    
     EXPR_END  = 0x06,
 
-    EP        = 0x11,
     E         = 0x12,
     T         = 0x13,
     F         = 0x14
@@ -39,16 +46,6 @@ enum Data_types
 
     SPACE_SYMB      = 0x53,
     SYNTAX_NOT_DET  = 0x54,
-};
-
-enum Operations
-{
-    PLUS        = 0x61,
-    MINUS       = 0x62,
-    UMNOZH      = 0x71,
-    DELIT       = 0x72,
-
-    END         = 0x81
 };
 
 #endif
